d23954f33490c0326b0dd7675e4393f571667413
Mario R <mario.reyes.mt@correo.usa.edu.co>
2021-11-04 14:43:48 -0500

    frontend reto3y4m

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -223,7 +223,7 @@ function borrarPalco(idElemento){
 
 
 
-///////CLIENTES///////////
+///////CLIENTE///////////
 
 function traerInformacionClientes(){
     $.ajax({
@@ -242,6 +242,7 @@ function pintarRespuesta2(respuesta){
     let myTable="<table>";
     for(i=0;i<respuesta.length;i++){
         myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].idClient+"</td>";
         myTable+="<td>"+respuesta[i].email+"</td>";
         myTable+="<td>"+respuesta[i].password+"</td>";
         myTable+="<td>"+respuesta[i].name+"</td>";
@@ -260,7 +261,7 @@ function guardarInformacionClientes(){
         password:$("#Clpassword").val(),
         name:$("#Clname").val(),
         age:$("#Clage").val(),
-        category:$("#Clcategory").val(),
+        category:$("#Pcategory").val(),
         };
       
         $.ajax({
@@ -333,6 +334,236 @@ function borrarCliente(idElemento){
         success:function(respuesta){
             $("#resultado").empty();
             traerInformacionClientes();
+            alert("Se ha Eliminado.")
+        }
+    });
+
+}
+
+
+/////////////////////MENSAJE/////////////////////
+
+
+
+
+function traerInformacionMensajes(){
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Message/all", 
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta3(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta3(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].idMessage+"</td>";
+        myTable+="<td>"+respuesta[i].messageText+"</td>";
+        function pintarRespuesta(name) {            
+        }
+        myTable+="<td> <button onclick=' actualizarInformacionMensajes("+respuesta[i].id+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarMensaje("+respuesta[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    } 
+    myTable+="</table>";
+    $("#resultado4").html(myTable);
+}
+
+function guardarInformacionMensajes(){
+    let var2 = {
+        messageText:$("#MmessageText").val(),
+        category:$("#Mcategory").val(),
+        
+        };
+        function pintarRespuesta(name) {            
+        }
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://144.22.247.183:8080/api/Message/save",
+       
+        
+        success:function(response) {
+            console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()  
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");  
+        }
+        });
+
+}
+
+function actualizarInformacionMensajes(idElemento){
+    let myData={
+        id:idElemento,
+        messageText:$("#MmessageText").val(),
+        category:$("#Mcategory").val(),
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Message/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#MmessageText").val("");
+            traerInformacionMensajes();
+            alert("se ha Actualizado correctamente el mensaje")
+        }
+    });
+
+}
+
+function borrarMensaje(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Message/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacionMensajes();
+            alert("Se ha Eliminado.")
+        }
+    });
+
+}
+
+
+
+
+
+/////////RESERVACIÓN///////////////
+
+
+
+
+function traerInformacionReservaciones(){
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Reservation/all", 
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta4(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta4(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].idElemento+"</td>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="<td> <button onclick=' actualizarInformacionReservaciones("+respuesta[i].id+")'>Actualizar</button>";
+        myTable+="<td> <button onclick='borrarReservacion("+respuesta[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    } 
+    myTable+="</table>";
+    $("#resultado5").html(myTable);
+}
+
+function guardarInformacionReservaciones(){
+    let var2 = {
+        name:$("#Rname").val(),
+        startDate:$("#RstartDate").val(),
+        devolutionDate:$("#RdevolutionDate").val(),
+        category:$("#Rcategory").val(),
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://144.22.247.183:8080/api/Reservation/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()  
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");  
+        }
+        });
+
+}
+
+function actualizarInformacionReservaciones(idElemento){
+    let myData={
+        id:idElemento,
+        startDate:$("#RstartDate").val(),
+        devolutionDate:$("#RdevolutionDate").val(),
+        category:$("#Rcategory").val(),
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Reservation/update",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            startDate:$("#RstartDate").val(),
+            // devolutionDate:$("#RdevolutionDate").val(),
+            traerInformacionReservaciones();
+            alert("se ha Actualizado correctamente")
+        }
+    });
+
+}
+
+function borrarReservacion(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"http://144.22.247.183:8080/api/Reservation/"+idElemento,
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacionReservaciones();
             alert("Se ha Eliminado.")
         }
     });
